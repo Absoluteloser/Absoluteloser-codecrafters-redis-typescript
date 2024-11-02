532f553ce69e5d4735ad7171832889e30acf215a
absoluteloser <balaji_motkulwar_comp@moderncoe.edu.in>
2024-11-02 17:18:13 +0000

    codecrafters submit [skip ci]

--- app/main.ts
+++ app/main.ts
@@ -6,6 +6,13 @@ console.log("Logs from your program will appear here!");
 // Uncomment this block to pass the first stage
 const server: net.Server = net.createServer((connection: net.Socket) => {
     // Handle connection
+    connection.on("data", (data: Buffer) => {
+        console.log(`receieved data: ${data.toString()}`)
+        console.log(`data received ${data}`)
+        console.log(`data converted to string ${data.toString()}`)
+        // connection.write(data.toString())
+        connection.write("+PONG\r\n")
+    })
 });
 
 server.listen(6379, "127.0.0.1");
