c3500ec1d4e448c9b8aaa5c82ae72629cd31498d
absoluteloser <balaji_motkulwar_comp@moderncoe.edu.in>
2024-11-02 18:32:47 +0000

    codecrafters submit [skip ci]

--- app/main.ts
+++ app/main.ts
@@ -2,17 +2,29 @@ import * as net from "net";
 
 // You can use print statements as follows for debugging, they'll be visible when running tests.
 console.log("Logs from your program will appear here!");
+const noOfOccurences = (str: string, findstr: string):number => {
+    const idx = str.indexOf(findstr)
+    let count=0
+    while (idx !== -1) {
+        count++
+        str.indexOf(findstr,idx+findstr.length)
+    }
+    return count
+}
 
 // Uncomment this block to pass the first stage
 const server: net.Server = net.createServer((connection: net.Socket) => {
     // Handle connection
     connection.on("data", (data: Buffer) => {
-        console.log(`receieved data: ${data.toString()}`)
-        console.log(`data received ${data}`)
-        console.log(`data converted to string ${data.toString()}`)
-        // connection.write(data.toString())
-        connection.write("+PONG\r\n")
+        // console.log(`receieved data: ${data.toString()}`)
+        // console.log(`data received ${data}`)
+        // console.log(`data converted to string ${data.toString()}`)
+        // connection.write("+PONG\r\n")
+        //operations to be performed on data
+        let command: string = data.toString()
+        if (command.includes("PING")) {
+            connection.write("+PONG\r\n")
+        }
     })
 });
-
 server.listen(6379, "127.0.0.1");
