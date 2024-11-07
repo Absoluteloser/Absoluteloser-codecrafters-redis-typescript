62a48d742fe699eb7431fa8342477f2f7327d50a
absoluteloser <balaji_motkulwar_comp@moderncoe.edu.in>
2024-11-07 19:31:04 +0000

    codecrafters submit [skip ci]

--- app/main.ts
+++ app/main.ts
@@ -1,7 +1,7 @@
 import * as net from "net";
 
 // You can use print statements as follows for debugging, they'll be visible when running tests.
-const store = new Map<string, string>();
+const store = new Map<string, { value: string, expiresAt: number | null }>();
 console.log("Logs from your program will appear here!");
 const noOfOccurences = (str: string, findstr: string): number => {
     const idx = str.indexOf(findstr)
@@ -22,6 +22,9 @@ const server: net.Server = net.createServer((connection: net.Socket) => {
         // console.log(`data converted to string ${data.toString()}`)
         // connection.write("+PONG\r\n")
         //operations to be performed on data
+        //data variable will contain the command
+        //$ redis-cli SET foo bar px 100 
+        //if this is the command then data is SET foo bar px 100
         let command: string = data.toString()
         if (command.includes("PING")) {
             connection.write("+PONG\r\n")
@@ -37,17 +40,38 @@ const server: net.Server = net.createServer((connection: net.Socket) => {
         if (command.includes("SET")) {
             const tostring: string = data.toString();
             const removedspaces: string[] = tostring.split("\r\n");
+            //*3\r\n$3\r\nSET\r\n$3\r\nkey\r\n$5\r\nvalue\r\n
             const key: string = removedspaces[4];
             const value: string = removedspaces[6];
-            store.set(key, value);
+            //map has set and get methods
+            // connection.write(`+OK\r\n`);
+            let expiresAt: number | null = null;
+            if (command.includes("px")) {
+                const dtostring: string = data.toString();
+                const modified: string[] = dtostring.split("\r\n");
+                const pxIndex: number = modified.indexOf("px");
+                const timediffinstring: string = modified[pxIndex + 2];
+                //time period is in milliseconds
+                const timediff: number | null = parseInt(timediffinstring);
+                expiresAt = Date.now() + timediff;
+            }
+            store.set(key, { value, expiresAt });
             connection.write(`+OK\r\n`);
         }
         if (command.includes("GET")) {
             const commandarr: string[] = command.split("\r\n");
             const key: string = commandarr[4];
-            const value = store.get(key);
-            if (value !== undefined) {
-                connection.write(`$${value.length}\r\n${value}\r\n`);
+            const entry = store.get(key);
+            if (entry) {
+                if (entry.expiresAt === null || entry.expiresAt > Date.now()) {
+                    connection.write(`$${entry.value.length}\r\n${entry.value}\r\n`);
+                }
+                else {
+                    //it gets expired delete
+                    store.delete(key);
+                    connection.write("$-1\r\n");
+                }
+
             }
             else {
                 connection.write("$-1\r\n");
